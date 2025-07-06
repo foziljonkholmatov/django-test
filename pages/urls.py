7570b745eb161c99a3d233dd0d2de642a57ce6e6
foziljonkholmatov <fozil77739@gmail.com>
2025-07-06 22:37:45 +0500

    my_self and my_teacher pages updated

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import home_view, about_view, contact_view
+from .views import home_view, about_view, contact_view, my_self_view, my_teacher_view
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('about/', about_view, name='about'),
     path('contact/', contact_view, name='contact'),
+    path('my_self/', my_self_view, name='my_self' ),
+    path('my_teacher/', my_teacher_view, name='my_teacher'),
 
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 
+
 def home_view(request):
     return render(request, 'index.html')
+
 
 def about_view(request):
     return render(request, 'about.html')
 
+
 def contact_view(request):
     return render(request, 'contact.html')
+
+
+def my_self_view(request):
+    return render(request, 'my_self.html')
+
+
+def my_teacher_view(request):
+    return render(request, 'my_teacher.html')
